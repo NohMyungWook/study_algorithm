fab2f37e41d0f969e71d342a1e75969f736f9248
noah <jeongas9810@gmail.com>
2023-11-10 01:18:30 +0900

    solve Q1463, Q1654, Q1932, Q2193

---
+++ src/src/week3/Q1932.java
@@ -0,0 +1,63 @@
+package week3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q1932 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> arrList = new ArrayList<>();
+        int a, b;
+
+        int n = Integer.parseInt(br.readLine().trim());
+        int[][] cache = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().trim().split(" ");
+            for (String s : input) {
+                arrList.add(Integer.parseInt(s));
+            }
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                cache[i][j] = arrList.get(cnt);
+                cnt++;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                continue;
+            }
+            if (i == 1){
+                cache[i][0] = cache[0][0] + cache[i][0];
+                cache[i][1] = cache[0][0] + cache[i][1];
+                continue;
+            }
+            for (int j = 0; j <= i; j++) {
+                if (j == 0) {
+                    cache[i][j] = cache[i-1][j] + cache[i][j];
+                    continue;
+                }
+                if (j == i) {
+                    cache[i][j] = cache[i-1][j-1] + cache[i][j];
+                    continue;
+                }
+                a = cache[i-1][j] + cache[i][j];
+                b = cache[i-1][j-1] + cache[i][j];
+                cache[i][j] = Math.max(a, b);
+            }
+        }
+        int max = cache[n-1][0];
+        for (int i = 1; i < cache.length; i++) {
+            max = Math.max(max, cache[n-1][i]);
+        }
+        System.out.print(max);
+    }
+}
---
+++ src/src/week4/Q1654.java
@@ -0,0 +1,43 @@
+package week4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q1654 {
+    public static boolean check(long mid, List<Long> line, int n) {
+        long sum = 0;
+        for (long i : line) {
+            if (mid > i) {
+                return false;
+            }
+            sum += (i / mid);
+        }
+        return sum >= n;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<Long> line = new ArrayList<>();
+        long lo = 1, hi = Integer.MAX_VALUE + 1L;
+        long mid;
+
+        String[] first = br.readLine().trim().split(" ");
+
+        int k = Integer.parseInt(first[0]); // 가지고 있는 랜선의 개수
+        int n = Integer.parseInt(first[1]); // 필요한 랜선의 개수
+        for (int i = 0; i < k; i++) {
+            line.add(Long.parseLong(br.readLine().trim()));
+        }
+
+        while (lo + 1 < hi) {
+            mid = lo + hi >> 1;
+
+            if (check (mid, line, n)) lo = mid;
+            else hi = mid;
+        }
+        System.out.print(lo);
+    }
+}

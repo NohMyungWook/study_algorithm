f9e4030663f2b0b4883d648a6151eac8c615c254
noah <jeongas9810@gmail.com>
2023-12-15 08:50:20 +0900

    solve sproute problem in solved.ac

---
+++ src/src/solvedac/sprout/Q10699.java
@@ -0,0 +1,9 @@
+package solvedac.sprout;
+
+import java.time.LocalDate;
+
+public class Q10699 {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now());
+    }
+}
---
+++ src/src/solvedac/sprout/Q7287.java
@@ -0,0 +1,8 @@
+package solvedac.sprout;
+
+public class Q7287 {
+    public static void main(String[] args) {
+        System.out.println("28");
+        System.out.println("jeongas9810");
+    }
+}

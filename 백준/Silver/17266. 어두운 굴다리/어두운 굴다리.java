470742fce8f6f14631b92c70a8c00892b026c6a7
NohMW <jeongas9810@naver.com>
2024-02-02 11:10:40 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 300 ms, Memory: 25284 KB -BaekjoonHub

---
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] x;
+    static int MIN;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+
+        x = new int[M];
+
+        for (int i = 0; i < M; i++) {
+            x[i] = Integer.parseInt(st.nextToken());
+        }
+
+        MIN = x[0];
+
+        for (int i = 1; i < M; i++) {
+            int dis = (x[i] - x[i - 1] + 1) / 2;
+            MIN = Math.max(MIN, dis);
+        }
+
+        MIN = Math.max(MIN, N - x[M - 1]);
+        System.out.print(MIN);
+    }
+}
